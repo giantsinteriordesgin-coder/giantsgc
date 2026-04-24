9970c76233e5b37fbb073b426b66c138bb067db9
giantsinteriordesgin-coder <giantsinteriordesgin@gmail.com>
2026-04-25 00:35:25 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,19 @@ function applyLang(){
   document.querySelector("button").textContent =
     currentLang === "en" ? "AR" : "EN";
 }
+
+function revealOnScroll(){
+  const elements = document.querySelectorAll(".reveal");
+
+  elements.forEach(el=>{
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+
+    if(elementTop < windowHeight - 100){
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
